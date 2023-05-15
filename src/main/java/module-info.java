8eb0f5d78aff3c3714341b58e6de76dc00db68ea
Azam Alamov <alamovazamjon@gmail.com>
2023-05-15 17:26:43 +0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.alamov.flymetothemoon {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.alamov.flymetothemoon to javafx.fxml;
-    exports com.alamov.flymetothemoon;
-}
